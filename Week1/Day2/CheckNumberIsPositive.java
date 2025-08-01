289ea78cf122aea89e02d9e5aafa341f5ce15640
Priyanka R <mailtopriyasprofile@gmail.com>
2025-08-02 02:11:49 +0530

    Add files via upload

---
+++ Week1/Day2/CheckNumberIsPositive.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// to check version 10 is positive
+		//declare
+		int version=10;
+		if(version>0) {
+			System.out.println("The Number is Positive");
+			
+		}
+		else {
+			System.out.println("The Number is Negative");
+		}
+			
+
+	}
+
+}
