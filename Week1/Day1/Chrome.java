af4dd3bdab7ef7f6e97a72a97e8c619884cb8be9
Priyanka R <mailtopriyasprofile@gmail.com>
2025-08-02 02:02:37 +0530

    Add files via upload

---
+++ Week1/Day1/Chrome.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// declare and initialize the following variables
+
+		// float - 91.0
+		float version = 91.0F;
+		System.out.println(version);
+
+		// String -Google
+		String developer = "Google";
+		System.out.println(developer);
+
+		// boolean -false
+		boolean isBeta = false;
+		System.out.println(isBeta);
+
+		// int -2008
+		int releaseYear = 2008;
+		System.out.println(releaseYear);
+
+		// char
+		char shortcutKey = 'C';
+		System.out.println(shortcutKey);
+
+	}
+
+}
